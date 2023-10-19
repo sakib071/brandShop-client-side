2a16935709746a7d66d099fd6910d895884192a6
sakib071 <c193055@ugrad.iiuc.ac.bd>
2023-10-20 01:18:30 +0600

    speciality section added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import BrandCards from "./BrandCards";
+import Specialty from "./Specialty";
 
 const Home = () => {
 
@@ -7,15 +8,32 @@ const Home = () => {
     console.log(products);
 
     return (
-        <div>
-            <div className='container flex flex-col items-center mt-44 mx-auto'>
+        <div className="min-h-screen pb-16">
+            <div className="carousel w-full mt-16">
+                <div id="item1" className="carousel-item w-full h-[80vh]">
+                    <img src={"https://i.ibb.co/DMxz5v6/image.png"} className="w-full" />
+                </div>
+                <div id="item2" className="carousel-item w-full">
+                    <img src={"https://i.ibb.co/z6nzBx5/image.png"} className="w-full" />
+                </div>
+                <div id="item3" className="carousel-item w-full">
+                    <img src={"https://i.ibb.co/fkMwztx/image.png"} className="w-full" />
+                </div>
+            </div>
+            <div className="flex justify-center w-full py-2 gap-2">
+                <a href="#item1" className="btn btn-xs">1</a>
+                <a href="#item2" className="btn btn-xs">2</a>
+                <a href="#item3" className="btn btn-xs">3</a>
+            </div>
+            <div className='container flex flex-col items-center mt-32 mx-auto'>
                 <h1 className='text-3xl font-bold'> WE Have <span className="text-blue-600">0{products.length}</span> Brands Available</h1>
-                <div className='grid md:grid-cols-3 gap-5 mb-24'>
+                <div className='grid md:grid-cols-3 gap-5'>
                     {
                         products.map(product => <BrandCards key={product.id} product={product}></BrandCards>)
                     }
                 </div>
             </div>
+            <Specialty></Specialty>
         </div>
 
 
